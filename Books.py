4b36814fa6b732763697fcc8f52f6dd87356ad61
Leonora Mae Schipper <elliemaegalyean@gmail.com>
2023-09-01 23:49:48 -0400

    .

--- Books.py
+++ Books.py
@@ -2,8 +2,8 @@ import BookPageMaker
 from utils import *
 from pprint import PrettyPrinter
 
-books_directory = Path("/home/ellie/vimwiki/Book Files/Books")
-books = [pull_YMF(i) for i in books_directory.glob("*")]
+books_directory = Path("./books/")
+books = [yaml.load(i.open()) for i in books_directory.glob("*")]
 book_list = BookPageMaker.make_book_list(books)
 print(f'\n\n{book_list}')
 
